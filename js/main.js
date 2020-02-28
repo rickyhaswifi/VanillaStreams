4efd8e8720354483e6360a71156117ee0a9c141c
Ricardo Bautista <rickyhaswifi@gmail.com>
2020-02-28 12:05:47 -0700

    new functions, ui improvments

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 NeedCode = () => {
  const Question =  document.getElementById('CodeSelect');
  const CodeResponse = document.getElementById('UPCWrap');
- const SongISRC = document.getElementById('SongISRC');
- Question.value === 'false' ? (CodeResponse.classList.remove("hidden"), SongISRC.classList.remove("hidden")) : (CodeResponse.classList = ("hidden"), SongISRC.classList = ("hidden"))
+ const SongISRC = document.getElementById('ISRCWrap');
+ Question.value === 'false' 
+ ? 
+ (CodeResponse.classList.remove("esconde"), SongISRC.classList.remove("esconde")) 
+ : 
+ (CodeResponse.classList = ("esconde"), SongISRC.classList = ("esconde"))
 }
 
 GenerateUPC = () => {
@@ -21,15 +25,20 @@ AddAlbum = () => {
   const AlbumUPCDisplay = document.getElementById('UPCDisplay');
   const LabelDisplay = document.getElementById('LabelDisplay');
   const Year = parseInt(ReleaseDate.value);
-  AlbumDetails.innerHTML = `${AlbumName.value} - ${ArtistName.value}`;
+  const UPCLabel = document.getElementById('UPCLabel');
+  AlbumDetails.innerHTML = AlbumName.length < 1 ? 'Add Album Details' : `${AlbumName.value} - ${ArtistName.value}`;
   LabelDisplay.innerHTML = `© ${Year} ${LabelName.value}`;
   ReleaseDateDisplay.innerHTML = `Release Date: ${ReleaseDate.value}`;
   AlbumUPCDisplay.innerHTML = (AlbumUPC.value > 0) ? AlbumUPC.value : GenerateUPC();
+  UPCLabel.innerHTML = AlbumUPC.value === 0  ? '' :`UPC: `;
 
   // Preview Modal
   const PreviewAlbumName = document.getElementById('PreviewAlbumName');
   const PreviewLabelDisplay = document.getElementById('PreviewLabelDisplay');
-  PreviewAlbumName.innerHTML = `${AlbumName.value} - ${ArtistName.value}`;
+  PreviewAlbumName.innerHTML = `
+  <h1 class="ui header">${AlbumName.value}</h1>
+  <h3 class="ui orange header">${ArtistName.value}</h3>
+  `;
   PreviewLabelDisplay.innerHTML = `© ${Year} ${LabelName.value}`;
   CustomID();
   ClearAlbumInputs();
@@ -62,8 +71,6 @@ proccessCover = () => {
 proccessCover();
 
 CustomID = () => {
-  const UPCLabel = document.getElementById('UPCLabel');
-  UPCLabel.innerHTML = `HI`;
   const CustomIDDisplay = document.getElementById('CustomIDDisplay');
   const LabelName = document.getElementById('LabelName');
   LabelRegex = LabelName.value.replace(/[\s]|[^A-Za-z0-9]/g, '');
@@ -75,7 +82,6 @@ GenerateStorePreview = () => {
 }
 
 ClearAlbumInputs = () => {
-  document.getElementById('UPCLabel').innerHTML = '';
   document.getElementById('AlbumName').value = '';
   document.getElementById('ArtistName').value = '';
   document.getElementById('ReleaseDate').value = '';
@@ -95,6 +101,9 @@ ResetAlbum = () => {
   document.getElementById('UPCDisplay').innerHTML = ``;
   document.getElementById('LabelDisplay').innerHTML = ``;
   document.getElementById('CustomIDDisplay').innerHTML = ``;
+  document.getElementById("UPCResponse").classList = ""
+  document.getElementById("UPCResponse").innerHTML = ""
+  document.getElementById('AddAlbumValidate').classList.remove('disabled');;
 }
 
 GenerateISRC = () => {
@@ -104,19 +113,29 @@ GenerateISRC = () => {
 
 const Songs = [];
 
+getUniqId = () => {
+  //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+ }
+
 ExampleSongs = () => {
   Songs.push(
   {Name:'Bohemian Rhapsody',
   ISRC: GenerateISRC(),
   Writer:'Freddy Mercury', 
+  id:getUniqId(),
   },
   {Name:'Under Pressure',
   ISRC:GenerateISRC(),
   Writer:'Freddy Mercury', 
+  id:getUniqId(),
   },
   {Name:`Don't Stop Me Now`,
   ISRC:GenerateISRC(),
   Writer:'Freddy Mercury', 
+  id:getUniqId(),
   })
   GetSongs();
 }
@@ -128,7 +147,8 @@ AddSong = () => {
  const newSong = ({
    Name: SongName.value, 
    ISRC: SongISRC.value < 1 ? GenerateISRC() : SongISRC.value, 
-   Writer: SongWriter.value.length < 1 ? 'No Writter Added' : SongWriter.value
+   Writer: SongWriter.value.length < 1 ? 'No Writter Added' : SongWriter.value,
+   id:getUniqId(),
   });
 
   Songs.push(newSong);
@@ -142,13 +162,13 @@ GetSongs = () => {
   const SongListModal = document.getElementById("SongListModal");
   var i = 1;
 
-  SongList.innerHTML = '<ol>' + Songs.map(function (Songs) {
+  SongList.innerHTML = Songs < 1 ?  'Add Some Songs Below' : '<ol>' + Songs.map(function (Songs) {
     return `<li> 
     <h1> ${Songs.Name} </h1> 
     <h2> ISRC:  ${Songs.ISRC} </h2> 
     <h3> Writer:  ${Songs.Writer} </h2> 
-    <!-- <button onclick="EditSong()" class="boton edit">Edit Song</button> -->
-    <button onclick="DeleteSong()" class="boton remove">Delete Song</button>
+    <button onclick="EditSong()" class="ui button inverted yellow">Edit Song</button>
+    <button onclick="DeleteSong()" class="ui button inverted red">Delete Song</button>
     </li>`;
   }).join('') + '</ol>';
 
@@ -162,13 +182,14 @@ SongListModal.innerHTML = `<table class='ui table'>
 </tr>
 ` + Songs.map(function (Songs) {
     return `
-    <tr> 
+    <tr id='PlayHover'> 
     <td> ${i++}.</td>
     <td> ${Songs.Name} </td>
     <td> ${GenerateTimeStamp()}</td> 
     <td> <i class="disabled play circle icon"> </td> 
     <td> <button style='float:right' onclick="" class="ui button orange">$1.29</button> </td> 
-    </tr>`;
+    </tr>
+    `;
   }).join('') + '</table>';
 }
 
@@ -180,29 +201,41 @@ GenerateTimeStamp = () => {
 }
 
 RandomSongs = () => {
-  Songs.shuffle();
+  Songs.sort(() => Math.random() - 0.5);
+   GetSongs();
 }
 
-EditSong = (id) => {
-  const SongName = document.getElementById('SongName');
-  const SongISRC = document.getElementById('SongISRC');
-  const SongWriter = document.getElementById('SongWriter');
+// EditSong = (id) => {
+//   const SongName = document.getElementById('SongName');
+//   const SongISRC = document.getElementById('SongISRC');
+//   const SongWriter = document.getElementById('SongWriter');
   
-  Songs.map(function (s){
-    if (s.id === id) {
-      //return id
+//   Songs.map(function (s){
+//     if (s.id === id) {
+//     Songs.splice(s.id, 0);
+//     SongName.value = s.Name;
+//     SongISRC.value = s.ISRC;
+//     SongWriter.value = s.Writer;
+//     }
+//     else{
+//       return s
+//       // alert('not')
+//     }
+//   })
+//   GetSongs();
+// }
 
-    SongName.value = s.Name;
-    SongISRC.value = s.ISRC;
-    SongWriter.value = s.Writer;
-    }
-    else{
-      return s
-      // alert('not')
-    }
-  })
-  GetSongs();
-}
+// EditSong = (id) => {
+//   const updatedSong = {
+//   Name: document.getElementById('SongName').value = '',
+//   ISRC: document.getElementById('SongISRC').value = '',
+//   Writer: document.getElementById('SongWriter').value = '',
+//   }
+//   const id = updatedSong.id
+//   const index = Songs.findIndex(song => song.id === id)
+//   Songs.splice(index, 1, updatedSong)
+//   console.log(Songs)
+//  }
 
 DeleteSong = (id) => {
   Songs.splice(id, 1);
@@ -213,6 +246,8 @@ ClearFields = () => {
 document.getElementById('SongName').value = '';
 document.getElementById('SongISRC').value = '';
 document.getElementById('SongWriter').value = '';
+document.getElementById("ISRCResponse").innerHTML = ""
+document.getElementById("ISRCResponse").classList = ""
 }
 
 GetSongs();
@@ -220,3 +255,77 @@ GetSongs();
 $('#previewModal').click(function() {
   $('.ui.modal').modal('show');
 })
+
+// VALIDATIONS
+
+ValidateUPC = () => {
+  const AddAlbumValidate = document.getElementById('AddAlbumValidate');
+  const UPC = document.getElementById("AlbumUPC").value;
+  const UpcOutput = document.getElementById("UPCvalid");
+  const Valid = UPC.length;
+  const regDigitUPC = /\d{12}/
+  const regDigitEAN = /\d{13}/
+  const regAlpha = /\D/
+  const redMadeup = /000|123|0123|45678|\b(\d\d)\1+\b/
+
+  if (redMadeup.test(UPC) === true) {
+    UpcOutput.innerHTML = `UPC may be made up`;
+    document.getElementById("UPCResponse").classList = "ui inverted segment yellow"
+    AddAlbumValidate.classList.add('disabled');
+  } 
+  else if (regDigitUPC.test(UPC) && Valid === 12 ) {
+    UpcOutput.innerHTML = `UPC is Valid at ${Valid} charecters long`;
+    document.getElementById("UPCResponse").classList = "ui inverted segment green"
+    AddAlbumValidate.classList.remove('disabled');
+  } 
+  else if (regDigitEAN.test(UPC) && Valid === 13 ) {
+    UpcOutput.innerHTML = `Input is an EAN with ${Valid}`;
+    document.getElementById("UPCResponse").classList = "ui inverted segment blue"
+    AddAlbumValidate.classList.remove('disabled');
+  } 
+  else if (regAlpha.test(UPC) === true) {
+    UpcOutput.innerHTML = `UPC invalid contains charecters, UPC can only contain digits`;
+    document.getElementById("UPCResponse").classList = "ui inverted segment red"
+    AddAlbumValidate.classList.add('disabled');
+  } 
+  else {
+    UpcOutput.innerHTML = `UPC is invalid, a valid UPC is 12 digits`;
+    document.getElementById("UPCResponse").classList = "ui inverted segment red"
+    AddAlbumValidate.classList.add('disabled');
+  }
+}
+
+ValidateISRC = () => {
+  const SongSubmit = document.getElementById('SongSubmit');
+  const ISRC = document.getElementById("SongISRC").value;
+  const ISRCOutput = document.getElementById("ISRCvalid");
+  const Valid = ISRC.length;
+  const regISRC = /[a-zA-Z]{5}\d{7}/;
+  const resInvalidISRC = /abc|ABC|123|0123|1234/;
+
+  if (resInvalidISRC.test(ISRC) === true) {
+    ISRCOutput.innerHTML = `ISRC may be made up`;
+    document.getElementById("ISRCResponse").classList = "ui inverted segment yellow"
+    SongSubmit.classList.add('disabled');
+  } 
+  else if (regISRC.test(ISRC) === true && Valid === 12 ) {
+    ISRCOutput.innerHTML = `ISRC is valid`;
+    document.getElementById("ISRCResponse").classList = "ui inverted segment green"
+    SongSubmit.classList.remove('disabled');
+  } 
+  else if (Valid < 12 ) {
+    ISRCOutput.innerHTML = `ISRC is invalid: Valid ISRC only 12 charecters, ${Valid} above`;
+    document.getElementById("ISRCResponse").classList = "ui inverted segment red"
+    SongSubmit.classList.add('disabled');
+  } 
+  else if (Valid > 12 ) {
+    ISRCOutput.innerHTML = `ISRC is invalid: Valid ISRC only 12 charecters, ${Valid} above`;
+    document.getElementById("ISRCResponse").classList = "ui inverted segment red"
+    SongSubmit.classList.add('disabled');
+  } 
+  else {
+    ISRCOutput.innerHTML = `ISRC is invalid`;
+    document.getElementById("ISRCResponse").classList = "ui inverted segment red"
+    SongSubmit.classList.add('disabled');
+  }
+}
